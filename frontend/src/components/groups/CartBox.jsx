0c8cff6ab2bfda8713cddc995c4e80068e5f5aba
Pratik Sangami <pratik.sangami@gurzu.com>
2021-03-22 13:26:11 +0545

    patch-UIenhancements

--- frontend/src/components/groups/CartBox.jsx
+++ frontend/src/components/groups/CartBox.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import CartItem from "../unit/Cartitem";
 import { useSelector, useDispatch } from "react-redux";
+import EmptyCarticon from "../../assets/img/empty_cart.svg";
 
 export default function CartBox() {
+  const empty_cart = require("../../assets/img/empty_cart.svg");
   const cartItems = useSelector((state) => state.cart.cartItems);
   if (cartItems.length > 0) {
     return cartItems.map((item) => (
@@ -10,9 +12,18 @@ export default function CartBox() {
     ));
   } else {
     return (
-      <p className="text-center" style={{ fontSize: "1.2rem", color: "grey" }}>
-        No items in the cart
-      </p>
+      <div
+        className="text-center"
+        style={{ fontSize: "1.2rem", color: "grey", marginTop: 50 }}
+      >
+        <img
+          src={empty_cart.default}
+          alt="empty-cart-icon"
+          className="mx-auto"
+          width="140"
+        />
+        <p>No items in the cart</p>
+      </div>
     );
   }
 }
